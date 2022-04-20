f146559b3a92b7bbac47bf085644a1baa3fc2c96
Skylar Hill <stellarskylark@gmail.com>
2022-04-19 22:16:41 -0500

    Add different voices and display parameters for the different characters

--- assets/dialogue/sample-dialogue.ink
+++ assets/dialogue/sample-dialogue.ink
@@ -4,7 +4,7 @@ Residence of Monsieur Phileas Fogg.
 
 === london ===
 Monsieur Phileas Fogg returned home early from the Reform Club, and in a new-fangled steam-carriage, besides!  
-"Passepartout," said he. "We are going around the world!" #yellow
+"Passepartout," said he. "We are going around the world!" #green
 
 + "Around the world, Monsieur?"
 	I was utterly astonished. 
@@ -14,9 +14,9 @@ Monsieur Phileas Fogg returned home early from the Reform Club, and in a new-fan
 
 === astonished ===
 "You are in jest!" I told him in dignified affront. "You make mock of me, Monsieur."
-"I am quite serious." #yellow
+"I am quite serious." #orange
 
-+ "But of course"
++ "But of course."
 	-> ending
 
 
@@ -26,5 +26,5 @@ I nodded curtly, not believing a word of it.
 
 
 === ending
-"We shall circumnavigate the globe within eighty days." He was quite calm as he proposed this wild scheme. "We leave for Paris on the 8:25. In an hour." #yellow
+"We shall circumnavigate the globe within eighty days." He was quite calm as he proposed this wild scheme. "We leave for Paris on the 8:25. In an hour." #blue
 -> END
--- scripts/dialogue.gd
+++ scripts/dialogue.gd
@@ -3,17 +3,18 @@ extends CanvasLayer
 export(Resource) var ink_script
 export(int) var print_speed = 20
 
+var orange_voice
+var green_voice
+var blue_voice
+var narrator_voice
+
 var target_text = ""
 var index = 0
 var time_since_last = 0.0
-var voice = ""
+var voice
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Ink.InkFile = ink_script
-	$Ink.LoadStory()
-	$Ink.Continue()
-	
 	# set up FMOD
 	Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_STEREO, 0)
 	Fmod.init(1024, Fmod.FMOD_STUDIO_INIT_LIVEUPDATE, Fmod.FMOD_INIT_NORMAL)
@@ -21,21 +22,34 @@ func _ready():
 	# load banks
 	Fmod.load_bank("res://assets/audio/Banks/Desktop/Master.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
 	Fmod.load_bank("res://assets/audio/Banks/Desktop/Master.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
-	Fmod.load_bank("res://assets/audio/Banks/Desktop/Dialogue.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://assets/audio/Banks/Desktop/Voices.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
 
 	# register listener
 	Fmod.add_listener(0, self)
+	
+	# register voices
+	orange_voice = Fmod.create_event_instance("event:/Voices/Orange")
+	green_voice = Fmod.create_event_instance("event:/Voices/Green")
+	blue_voice = Fmod.create_event_instance("event:/Voices/Blue")
+	narrator_voice = Fmod.create_event_instance("event:/Voices/Narrator")
+	
+	# Register Ink story
+	$Ink.InkFile = ink_script
+	$Ink.LoadStory()
+	$Ink.Continue()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if $Dialogue.bbcode_text.length() == target_text.length():
+		if Fmod.get_event_playback_state(voice) == 0:
+			Fmod.stop_event(voice, Fmod.FMOD_STUDIO_STOP_ALLOWFADEOUT)
 		return
 	if time_since_last > 1.0 / print_speed:
 		index += 1
 		$Dialogue.bbcode_text = target_text.left(index)
 		time_since_last = 0.0
-		Fmod.play_one_shot("event:/Voices/%s" % voice, self)
 	time_since_last += delta
 
 
@@ -47,16 +61,23 @@ func _on_Ink_InkChoices(choices):
 
 func _on_Ink_InkContinued(text, tags):
 	var speaker = ""
-	if tags and tags[0] == "yellow":
-		voice = "Narrator" # TODO: Make Yellow voice and update tthis value
-		speaker = "[color=yellow][i]Yellow[/i][/color]\n"
+	if tags and tags[0] == "orange":
+		voice = orange_voice
+		speaker = "[color=#FF8C00][i]Orange[/i][/color]\n"
+	elif tags and tags[0] == "green":
+		voice = green_voice
+		speaker = "[color=green][i]Green[/i][/color]\n"
+	elif tags and tags[0] == "blue":
+		voice = green_voice
+		speaker = "[color=blue][i]Blue[/i][/color]\n"
 	else:
-		voice = "Narrator"
+		voice = narrator_voice
 		speaker = "\n"
 	index = speaker.length()
 	target_text = speaker + text
 	print(target_text)
 	time_since_last = 0.0
+	Fmod.start_event(voice)
 
 
 func _on_Dialogue_gui_input(event):
@@ -65,6 +86,7 @@ func _on_Dialogue_gui_input(event):
 			if $Dialogue.bbcode_text.length() < target_text.length():
 				$Dialogue.bbcode_text = target_text
 				return
+			Fmod.stop_event(voice, Fmod.FMOD_STUDIO_STOP_ALLOWFADEOUT)
 			$Ink.Continue()
 
 
