adba6de6232bc827d97a6e0387aa61c937d1792f
Skylar Hill <stellarskylark@gmail.com>
2022-05-02 18:19:39 -0500

    minor dialogue refinements, add movement particles

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
+export(PackedScene) var particles
 var velocity = Vector2.ZERO
-var move_speed = 1800
+var move_speed = 2400
 var input_locked = false
 var won = false
 
@@ -18,16 +19,32 @@ func _process(delta):
 		if Input.is_action_just_pressed("move_up"):
 			velocity = Vector2(0, -move_speed)
 			input_locked = true
+			
+			var puff = particles.instance()
+			puff.position = self.position
+			get_owner().add_child(puff)
 		elif Input.is_action_just_pressed("move_down"):
 			velocity = Vector2(0, move_speed)
 			input_locked = true
+			
+			var puff = particles.instance()
+			puff.position = self.position
+			get_owner().add_child(puff)
 		elif Input.is_action_just_pressed("move_right"):
 			velocity = Vector2(move_speed, 0)
 			input_locked = true
+			
+			var puff = particles.instance()
+			puff.position = self.position
+			get_owner().add_child(puff)
 		elif Input.is_action_just_pressed("move_left"):
 			velocity = Vector2(-move_speed, 0)
 			input_locked = true
-	
+			
+			var puff = particles.instance()
+			puff.position = self.position
+			get_owner().add_child(puff)
+			
 	if input_locked and not move_and_slide(velocity):
 		Fmod.play_one_shot("event:/SFX/Collision", self)
 		velocity = Vector2.ZERO
